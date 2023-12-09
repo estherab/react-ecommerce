3b2488ad01c6bcb23ae5afaf1ef0993e2a693478
estherab <esther.abenito@gmail.com>
2023-12-09 20:40:16 +0100

    Add sidebar functionality

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,6 +9,8 @@ import { useProductsContext } from '../context/products_context'
 import { useUserContext } from '../context/user_context'
 
 const Nav = () => {
+  const {openSidebar} = useProductsContext();
+
   return <NavContainer>
     <div className='nav-center'>
       <div className='nav-header'>
@@ -16,7 +18,7 @@ const Nav = () => {
           <img src={logo} alt='Comfy Sloth'/>
         </Link>
 
-        <button type='button' className='nav-toggle'><FaBars /></button>
+        <button type='button' className='nav-toggle' onClick={openSidebar}><FaBars /></button>
       </div>
 
       <ul className='nav-links'>
