bbe5d210633cda3cd102b23c8d0df8a0ee1f53a1
Esther Avila <esther.abenito@gmail.com>
2024-01-24 08:24:54 +0100

    Add single product error and loading states

--- src/context/products_context.js
+++ src/context/products_context.js
@@ -68,7 +68,7 @@ export const ProductsProvider = ({ children }) => {
   }, [])
 
   return (
-    <ProductsContext.Provider value={{...state, openSidebar, closeSidebar}}>
+    <ProductsContext.Provider value={{...state, openSidebar, closeSidebar, fetchSingleProduct}}>
       {children}
     </ProductsContext.Provider>
   )
