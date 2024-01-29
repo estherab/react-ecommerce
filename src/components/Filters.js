bd008779ec597d3f74937f1640f3300afdc1d5ae
Esther Avila <esther.abenito@gmail.com>
2024-01-29 10:01:10 +0100

    Add filters functionality

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -5,7 +5,43 @@ import { getUniqueValues, formatPrice } from '../utils/helpers'
 import { FaCheck } from 'react-icons/fa'
 
 const Filters = () => {
-  return <h4>filters</h4>
+  const {
+    filters: {
+      text,
+      category,
+      company,
+      color,
+      min_price,
+      price,
+      max_price,
+      shipping,
+    },
+
+    updateFilters,
+    clearFilters,
+    all_products,
+  } = useFilterContext()
+
+  const categories = getUniqueValues(all_products, 'category')
+  const companies = getUniqueValues(all_products, 'company')
+  const colors = getUniqueValues(all_products, 'colors')
+
+  return <Wrapper>
+    <div className='content'>
+      <form onSubmit={(e) => e.preventDefault()}>
+        <div className='form-control'>
+          <input
+            type='text'
+            name='text'
+            placeholder='search'
+            className='search-input'
+            value={text}
+            onChange={updateFilters} 
+          />
+        </div>
+      </form>
+    </div>
+  </Wrapper>
 }
 
 const Wrapper = styled.section`
